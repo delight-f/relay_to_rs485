1cb4707abfdea172bcfce96eee678bc7124c7bd5
delight-f <76928118+delight-f@users.noreply.github.com>
2024-09-06 21:01:36 +1000

    More edits
    Change of library...

--- Firmware/Relay_to_RS458/src/main.cpp
+++ Firmware/Relay_to_RS458/src/main.cpp
@@ -1,31 +1,14 @@
-#include <ModbusMaster.h>
+#include <ModBusRTU.h>
+#include <ModbusAPI.h>
 #include <driver/gpio_filter.h>
 #include <driver/gpio.h>
 
-/* Modbus stuff */
-#define MODBUS_DIR_PIN  20 // connect DR, RE pin of MAX485 to gpio 20
-#define MODBUS_RX_PIN 16// Rx pin  
-#define MODBUS_TX_PIN 17 // Tx pin 
-#define MODBUS_SERIAL_BAUD 9600 // Baud rate for esp32 and max485 communication
+// initialise ModbusRTU
 
-//Initialize the ModbusMaster object as node
-ModbusMaster node;
-
-// Pin 20 made high for Modbus transmision mode
-void modbusPreTransmission()
-{
-  delay(3);
-  digitalWrite(MODBUS_DIR_PIN, HIGH);
-}
-// Pin 20 made low for Modbus receive mode
-void modbusPostTransmission()
-{
-  digitalWrite(MODBUS_DIR_PIN, LOW);
-  delay(3);
-}
+ModbusRTU mb;
 
 // Define the digital input pins for the input relays
-const int inputPins[5] = {2, 3, 4, 5, 6};
+const int inputPins[5] = {2, 4, 5, 6, 7};
 
 // Define the MODBUS coil addresses for the relays
 const int coilAddresses[5] = {0x0000, 0x0001, 0x0002, 0x0003, 0x0004};
@@ -35,44 +18,33 @@ void setup()
     // Initialize serial communication
     Serial.begin(115200);
 
-    pinMode(MODBUS_DIR_PIN, OUTPUT);
-    digitalWrite(MODBUS_DIR_PIN, LOW);
+    //Serial1.begin(baud-rate, protocol, RX pin, TX pin);.
+    Serial1.begin(9600,SERIAL_8E1, 37, 39);
+    Serial1.setTimeout(200);
 
-    //Serial0.begin(baud-rate, protocol, RX pin, TX pin);.
-    Serial0.begin(MODBUS_SERIAL_BAUD, SERIAL_8E1, MODBUS_TX_PIN, MODBUS_RX_PIN);
-    Serial0.setTimeout(200);
-    //modbus slave ID 255
-    node.begin(255, Serial0);
-    
+    //modbusRTU begin
+    mb.begin(&Serial1);
+    mb.client();
+     
     // Set input pins as input with internal pull-up resistors
     for (int pin : inputPins) {
     pinMode(pin, INPUT_PULLUP);
 
+
 }   // end input pin pull-up resistor routine
 
-// create glitch filters
-
-for (int i = 2; i <= 6; i++) {
-  gpio_pin_glitch_filter_config_t config = {
-    .gpio_num = (gpio_num_t)i
-  };
-  gpio_glitch_filter_handle_t filter_handle;
-  gpio_glitch_filter_enable(filter_handle);
-} //end glitch filter setup
-
-//  callbacks allow us to configure the RS485 transceiver correctly  
-    node.preTransmission(modbusPreTransmission);
-    node.postTransmission(modbusPostTransmission);
-    
 } // end initial setup
- void loop() 
-{
-  for (int i = 0; i < 5; i++) {
-    if (digitalRead(inputPins[i]) == HIGH) {
-      node.writeSingleCoil(coilAddresses[i], 0xFF);
-    } else {
-      node.writeSingleCoil(coilAddresses[i], 0x00);
+ void loop()  
+     {
+      for (int i = 0; i < 5; i++) 
+      {
+        if (digitalRead(inputPins[i]) == HIGH) {
+          mb.writeCoil(0xFF, coilAddresses[i], true);
+        } else {
+          mb.writeCoil(0xFF, coilAddresses[i], false);
+        }
+      }
+      delay(50); // Small delay to avoid excessive polling
+      mb.task();
+      yield;
     }
-  }
-  delay(100); // Small delay to avoid excessive polling
-}
