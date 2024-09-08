910ee6ede5302a4c26c8be0214616fbb6156b40c
delight-f <76928118+delight-f@users.noreply.github.com>
2024-09-08 19:24:46 +1000

    General formatting, Whitesmiths style

--- Firmware/Relay_to_RS458/src/main.cpp
+++ Firmware/Relay_to_RS458/src/main.cpp
@@ -13,7 +13,8 @@ const int inputPins[] = {7, 8, 9, 10, 11};
 const int coilAddresses[] = {0x0000, 0x0001, 0x0002, 0x0003, 0x0004};
 
 
-void setup() {
+void setup() 
+  {
   // Initialize serial communication
   Serial.begin(115200);
 
@@ -24,28 +25,32 @@ void setup() {
   node.begin(255,Serial1);
 
   // Set input pins as input with internal pull-up resistors
-  for (int i = 0; i < 5; i++) {
-    pinMode(inputPins[i], INPUT_PULLUP);
+  for (int i = 0; i < 5; i++) 
+    {
+      pinMode(inputPins[i], INPUT_PULLUP);
+    }
+
+  for (int i = 0; i < 5; i++) 
+    {
+      gpio_pin_glitch_filter_config_t config = {
+        .gpio_num = (gpio_num_t)inputPins[i]
+      };
+      gpio_glitch_filter_handle_t filter_handle;
+      gpio_glitch_filter_enable(filter_handle);
+    } //end glitch filter setup
+
+  }   // end input pin pull-up resistor routine
+
+
+void loop() 
+  {
+    for (int i = 0; i < 5; i++) 
+    {
+      int inputPin = inputPins[i];
+      int coilAddress = coilAddresses[i];
+
+      bool inputValue = digitalRead(inputPin) == HIGH;
+      bool valueToWrite = inputValue ? false : true;  // Explicitly map HIGH to false and LOW to true
+      node.writeSingleCoil(coilAddress, valueToWrite);
+    }
   }
-
-for (int i = 0; i < 5; i++) {
-  gpio_pin_glitch_filter_config_t config = {
-    .gpio_num = (gpio_num_t)inputPins[i]
-  };
-  gpio_glitch_filter_handle_t filter_handle;
-  gpio_glitch_filter_enable(filter_handle);
-} //end glitch filter setup
-
-}   // end input pin pull-up resistor routine
-
-
-void loop() {
-  for (int i = 0; i < 5; i++) {
-    int inputPin = inputPins[i];
-    int coilAddress = coilAddresses[i];
-
-    bool inputValue = digitalRead(inputPin) == HIGH;
-    bool valueToWrite = inputValue ? false : true;  // Explicitly map HIGH to false and LOW to true
-    node.writeSingleCoil(coilAddress, valueToWrite);
-  }
-}
